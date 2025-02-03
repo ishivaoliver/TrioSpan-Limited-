b27ac45a918ca38bd99e5c4e4f030c01030b645f
Ishiva Oliver <ishivaoliver@gmail.com>
2025-02-03 18:16:27 +0300

    about section styling

--- js/main.js
+++ js/main.js
@@ -17,3 +17,17 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Smooth Scroll Script
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});
+
+
+
