f9cdcd01561b7dc827e94937c4e92ad328d082ea
Ishiva Oliver <ishivaoliver@gmail.com>
2025-02-03 23:10:26 +0300

    back to top functionality and adjusting the height of the mapp

--- js/main.js
+++ js/main.js
@@ -44,6 +44,27 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// button back to top
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "flex";
+      backToTopButton.style.animation = "fadeIn 0.5s";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
+
 
 
 
